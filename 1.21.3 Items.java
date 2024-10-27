6ef98f6af07eefc8bc6239476082e85a8f720702
John <forddarwin6@gmail.com>
2024-10-27 13:48:41 -0500

    Update and rename 1.21.3 Items to 1.21.3 Items.java

--- 1.21.3 Items.java
+++ 1.21.3 Items.java
@@ -5,3 +5,15 @@ public static Item registerItem(String id, String name) { //found this from more
         Item.Settings settings = new Item.Settings().registryKey(key).translationKey(name);
         return Registry.register(Registries.ITEM, key, new Item(settings));
     }
+
+public static void initialize() {
+        Item[] customItems = {
+                registerItem("id", "name"),
+        };
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL)
+                .register(entries -> {
+                    entries.add(customItems[0]);
+                });
+
+    }
